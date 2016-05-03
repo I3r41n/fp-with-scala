67cbcfc071aab827d5ca8b2f963599979b487c97
Bruno Santos <bsantos@equalexperts.com>
2016-05-03 11:02:37 +0100

    Fibonacci test and tail rec

--- src/main/scala/com/exercises/chapter/two.scala
+++ src/main/scala/com/exercises/chapter/two.scala
@@ -1,19 +1,22 @@
 package com.exercises.chapter
 
+import scala.annotation.tailrec
+
 /**
   * Created by bsantos on 21/04/16.
   */
 object two {
   // Exercise 1: Write a function to compute the nth fibonacci number
 
- def fibonacci(n: Int): Int = {
-//  def aux (i: Int): Int = {
-//    if(1 >= i ) i
-//    else aux(i-1) + aux (i-2)
-//  }
-//
-//   aux(n)
-   0
- }
+  def fibonacci(n: Int): Int = {
 
+    @tailrec
+    def aux (i: Int, previousFib: Int, currentFib: Int): Int = i match {
+      case 0 => currentFib
+      case i  if i > 0 => aux(i-1, currentFib, previousFib + currentFib)
+      case _ =>  throw new RuntimeException
+    }
+
+    aux(n , 0 , 1)
+  }
 }
--- src/test/scala/com/exercises/chapter/twoTest.scala
+++ src/test/scala/com/exercises/chapter/twoTest.scala
@@ -1,19 +1,39 @@
 package com.exercises.chapter
 
-import org.scalatest.{FunSuite, WordSpec}
-import org.scalacheck.Gen.posNum
-import org.scalacheck.Prop._
-import org.scalatest.prop.Checkers
-import org.scalacheck.Test.check
 import com.exercises.chapter.two._
-
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
+import org.scalatest.FunSuite
+import org.scalatest.prop.Checkers
 
 class twoSpec extends FunSuite with Checkers{
+  private val smallPosFrom0: Gen[Int] = Gen.choose(0, 100)
+  private val smallPosFrom2: Gen[Int] = Gen.choose(2, 100)
 
+  test("Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square"){
+    val property = forAll(smallPosFrom0) { (int: Int) => {
+      val common = 5 * Math.pow(fibonacci(int), 2)
+      isPerfectSquare(common + 4) || isPerfectSquare(common - 4)
+    }}
+    check(property)
+  }
 
-  test("Fibonacci - each fibonacci number bigger than 1 is the sum of the previous two") {
-    val property = forAll(posNum[Int] suchThat ( _ > 1)){ (int: Int) =>
+  test("Fibonacci - each fibonacci number bigger than 3 is bigger than the previous one") {
+    val property =
+      forAll(smallPosFrom2){ (int: Int) =>
+      fibonacci(int) > fibonacci(int - 1)}
+    check(property)
+  }
+
+  test("Fibonacci - each fibonacci number bigger than 2 is the sum of the previous two") {
+    val property =
+      forAll(smallPosFrom2){ (int: Int) =>
       fibonacci(int) == fibonacci(int - 1) + fibonacci(int - 2)}
     check(property)
+  }
+
+  def isPerfectSquare(n: Double) = {
+    val s = Math.sqrt(n)
+    s * s == n
   }
 }
